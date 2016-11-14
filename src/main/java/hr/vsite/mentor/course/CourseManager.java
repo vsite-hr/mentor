8e216daed01684285ff91c42191840c09d7d5ef0
Dejan Barušić <dejan.barusic@gmail.com>
2016-11-14 14:06:45 +0100

    New method - public void insert(Course) - that inserts/updates Course in database

--- src/main/java/hr/vsite/mentor/course/CourseManager.java
+++ src/main/java/hr/vsite/mentor/course/CourseManager.java
@@ -24,37 +24,112 @@ public class CourseManager {
 		this.connProvider = connProvider;
 	}
 
-	/** Returns <code>Course</code> with corresponding id, or <code>null</code> if such course does not exist. */
+	/**
+	 * Inserts new <code>Course</code> in database if <code>course.getId()!=null</code> 
+	 * (course title, description and author must be other than <code>null</code>), 
+	 * or updates existing <code>Course</code>.
+	 */
+	public void insert(Course course) {
+
+		if (course.getId() == null) {
+			Log.info("Inserting course \"" + course.getTitle() + "\"...");
+			String query = "INSERT INTO courses VALUES (DEFAULT, ?, ?, UUID(?))";
+			try (PreparedStatement statement = connProvider.get().prepareStatement(query)) {
+				int index = 0;
+				if (course.getTitle() != null)
+					statement.setString(++index, course.getTitle());
+				else {
+					Log.info("Missing \"title\"");
+					return;
+				}
+				if (course.getDescription() != null)
+					statement.setString(++index, course.getDescription());
+				else {
+					Log.info("Missing \"description\"");
+					return;
+				}
+				if (course.getAuthor() != null)
+					statement.setString(++index, course.getAuthor().getId().toString());
+				else {
+					Log.info("Missing \"author\"");
+					return;
+				}
+				Log.info(statement.executeUpdate() + " rows inserted in database");
+			} catch (SQLException e) {
+				throw new RuntimeException("Unable to insert course", e);
+			}
+		} else {
+			Log.info("Updating course " + course.getId().toString() + "...");
+			StringBuilder queryBuilder = new StringBuilder(1000);
+			queryBuilder.append("UPDATE courses SET ");
+			if (course.getTitle() != null)
+				queryBuilder.append("course_title=?");
+			if (course.getTitle() != null && (course.getDescription() != null || course.getAuthor() != null))
+				queryBuilder.append(", ");
+			if (course.getDescription() != null)
+				queryBuilder.append("course_description=?");
+			if (course.getDescription() != null && course.getAuthor() != null)
+				queryBuilder.append(", ");
+			if (course.getAuthor() != null)
+				queryBuilder.append("author_id=UUID(?) ");
+			queryBuilder.append("WHERE course_id=UUID(?)");	
+			try (PreparedStatement statement = connProvider.get().prepareStatement(queryBuilder.toString())) {
+				int index = 0;
+				if (course.getTitle() != null)
+					statement.setString(++index, course.getTitle());
+				if (course.getDescription() != null)
+					statement.setString(++index, course.getDescription());
+				if (course.getAuthor() != null)
+					statement.setString(++index, course.getAuthor().getId().toString());
+				statement.setString(++index, course.getId().toString());
+				Log.info(statement.executeUpdate() + " rows updated in database");
+			} catch (SQLException e) {
+				throw new RuntimeException("Unable to update course", e);
+			}
+		}
+	}
+
+	/**
+	 * Returns <code>Course</code> with corresponding id, or <code>null</code>
+	 * if such course does not exist.
+	 */
 	public Course findById(UUID id) {
 
-		try (PreparedStatement statement = connProvider.get().prepareStatement("SELECT * FROM courses WHERE course_id = ?")) {
+		try (PreparedStatement statement = connProvider.get()
+				.prepareStatement("SELECT * FROM courses WHERE course_id = ?")) {
 			statement.setObject(1, id);
 			try (ResultSet resultSet = statement.executeQuery()) {
-	        	return resultSet.next() ? fromResultSet(resultSet) : null;
-	        }
+				return resultSet.next() ? fromResultSet(resultSet) : null;
+			}
 		} catch (Exception e) {
 			throw new RuntimeException("Unable to find course with id " + id, e);
 		}
-	
+
 	}
-	
-	/** Returns all courses known to application, unpaged. If there is a possibility for large number of courses,
-	 * use {@link #list(CourseFilter, Integer, Integer)} */
+
+	/**
+	 * Returns all courses known to application, unpaged. If there is a
+	 * possibility for large number of courses, use
+	 * {@link #list(CourseFilter, Integer, Integer)}
+	 */
 	public List<Course> list() {
 		return list(new CourseFilter(), null, null);
 	}
-	
-	/** Returns courses that match given criteria, unpaged. If there is a possibility for large number of courses,
-	 * use {@link #list(CourseFilter, Integer, Integer)} */
+
+	/**
+	 * Returns courses that match given criteria, unpaged. If there is a
+	 * possibility for large number of courses, use
+	 * {@link #list(CourseFilter, Integer, Integer)}
+	 */
 	public List<Course> list(CourseFilter filter) {
 		return list(filter, null, null);
 	}
-	
+
 	/** Returns courses that match given criteria, paged. */
 	public List<Course> list(CourseFilter filter, Integer count, Integer offset) {
 
 		List<Course> courses = new ArrayList<>(count != null ? count : 10);
-		
+
 		StringBuilder queryBuilder = new StringBuilder(1000);
 		queryBuilder.append("SELECT * FROM courses WHERE true");
 		if (filter.getTitle() != null)
@@ -62,42 +137,45 @@ public class CourseManager {
 		if (filter.getDescription() != null)
 			queryBuilder.append(" AND lower(course_description) LIKE '%' || lower(?) || '%'");
 		if (filter.getAuthor() != null)
-			queryBuilder.append(" AND lower(author_id) = lower(?)");
+			queryBuilder.append(" AND author_id=UUID(?)");
 		if (count != null)
 			queryBuilder.append(" LIMIT ?");
 		if (offset != null)
 			queryBuilder.append(" OFFSET ?");
-		
+
 		try (PreparedStatement statement = connProvider.get().prepareStatement(queryBuilder.toString())) {
 			int index = 0;
 			if (filter.getTitle() != null)
-			    statement.setString(++index, filter.getTitle());
+				statement.setString(++index, filter.getTitle());
 			if (filter.getDescription() != null)
-			    statement.setString(++index, filter.getDescription());
+				statement.setString(++index, filter.getDescription());
 			if (filter.getAuthor() != null)
-			    statement.setString(++index, String.class.cast(filter.getAuthor().getId()));
+				statement.setString(++index, filter.getAuthor().getId().toString());
 			if (count != null)
 				statement.setInt(++index, count);
 			if (offset != null)
 				statement.setInt(++index, offset);
-	        try (ResultSet resultSet = statement.executeQuery()) {
-	        	while(resultSet.next())
-	        		courses.add(fromResultSet(resultSet));
-	        }
+			try (ResultSet resultSet = statement.executeQuery()) {
+				while (resultSet.next())
+					courses.add(fromResultSet(resultSet));
+			}
 		} catch (SQLException e) {
 			throw new RuntimeException("Unable to list courses", e);
 		}
-		
-	    return courses;
-	    
+
+		return courses;
+
 	}
-	
-	/** Parses given ResultSet and extract {@link Course} from it.
-	 * If ResultSet had <code>NULL</code> in <code>course_id</code> column, <code>null</code> is returned. */
+
+	/**
+	 * Parses given ResultSet and extract {@link Course} from it. If ResultSet
+	 * had <code>NULL</code> in <code>course_id</code> column, <code>null</code>
+	 * is returned.
+	 */
 	public Course fromResultSet(ResultSet resultSet) {
-		
+
 		Course course = new Course();
-		
+
 		try {
 			course.setId(UUID.class.cast(resultSet.getObject("course_id")));
 			if (resultSet.wasNull())
@@ -108,14 +186,14 @@ public class CourseManager {
 		} catch (SQLException e) {
 			throw new RuntimeException("Unable to resolve course from result set", e);
 		}
-		
+
 		return course;
-		
+
 	}
-	
+
 	private static final Logger Log = LoggerFactory.getLogger(CourseManager.class);
 
 	private final Provider<UserManager> userProvider;
 	private final Provider<Connection> connProvider;
-	
+
 }
