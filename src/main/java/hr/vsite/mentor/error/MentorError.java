ec9e249b4601a157c78f028aaea7d818b17c4c10
Dejan Barušić <dejan.barusic@gmail.com>
2016-11-23 18:02:05 +0100

    Updated

--- src/main/java/hr/vsite/mentor/error/MentorError.java
+++ src/main/java/hr/vsite/mentor/error/MentorError.java
@@ -1,13 +1,12 @@
 package hr.vsite.mentor.error;
 
-public interface MentorError {
+public class MentorError {
 
-	public String getUserMessage();
-	public void setUserMessage(String userMessage);
-	public String getInternalMessage();
-	public void setInternalMessage(String internalMessage);
-	public int getCode();
-	public void setCode(int code);
-	public String getInfo();
-	public void setInfo(String info);
+	public int getCode() { return code; }
+	public void setCode(int code) { this.code = code; }
+	public String getMessage() { return message; }
+	public void setMessage(String message) { this.message = message; }
+	
+	private int code;
+	private String message;
 }
