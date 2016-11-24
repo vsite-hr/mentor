057b95bb47d55832bab771d596ef1e2e72f9b926
gkresic <gordan.kresic@steatoda.com>
2016-11-24 19:19:30 +0100

    More JSON annotations

--- src/main/java/hr/vsite/mentor/unit/MentorTextUnitAttributes.java
+++ src/main/java/hr/vsite/mentor/unit/MentorTextUnitAttributes.java
@@ -1,9 +1,13 @@
 package hr.vsite.mentor.unit;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class MentorTextUnitAttributes extends TextUnitAttributes {
 
+	@JsonProperty
 	public MarkupType getMarkupType() { return markupType; }
 	public void setMarkupType(MarkupType markupType) { this.markupType = markupType; }
+	@JsonProperty
 	public String getMarkup() { return markup; }
 	public void setMarkup(String markup) { this.markup = markup; }
 	
--- src/main/java/hr/vsite/mentor/unit/TextUnitAttributes.java
+++ src/main/java/hr/vsite/mentor/unit/TextUnitAttributes.java
@@ -1,7 +1,10 @@
 package hr.vsite.mentor.unit;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class TextUnitAttributes {
 
+	@JsonProperty
 	public TextUnit.Type getTextUnitType() { return textUnitType; }
 	public void setTextUnitType(TextUnit.Type textUnitType) { this.textUnitType = textUnitType; }
 
