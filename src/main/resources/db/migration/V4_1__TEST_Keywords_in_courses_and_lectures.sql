80fb80b582b43071b61bc4d22f4f44f774ae32f0
gkresic <gordan.kresic@steatoda.com>
2016-11-25 10:35:33 +0100

    Keywords for courses and lectures in db schema.

---
+++ src/main/resources/db/migration/V4_1__TEST_Keywords_in_courses_and_lectures.sql
@@ -0,0 +1,3 @@
+UPDATE public.courses SET course_keywords = '{"alternating current",AC,"electric current","electric charge"}' WHERE course_id = 'f0aabc82-a475-11e6-aedd-4485006d0fd8'::uuid;
+UPDATE public.lectures SET lecture_keywords = '{"power transmission","electrical energy","power plant"}' WHERE lecture_id = '200ceefa-a476-11e6-aedd-4485006d0fd8'::uuid;
+
