bb4b5cb1bfc53853a6a3b5111e5910069dee94f1
gkresic <gordan.kresic@steatoda.com>
2016-11-25 11:26:04 +0100

    Db schema support for head units in courses and lectures.

---
+++ src/main/resources/db/migration/V5_1__TEST_Head_units_in_courses_and_lectures.sql
@@ -0,0 +1,3 @@
+UPDATE public.courses SET course_head_unit_id = '8faae33a-b27b-11e6-aedd-4485006d0fd8'::uuid WHERE course_id = 'f0aabc82-a475-11e6-aedd-4485006d0fd8'::uuid;
+
+UPDATE public.lectures SET lecture_head_unit_id = '8faae33a-b27b-11e6-aedd-4485006d0fd8'::uuid WHERE lecture_id = '200ceefa-a476-11e6-aedd-4485006d0fd8'::uuid;
