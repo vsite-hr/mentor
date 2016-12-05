e9fc11f1c4b2390670a469be09247457f0036433
gkresic <gordan.kresic@steatoda.com>
2016-12-05 21:55:34 +0100

    Keywords in LectureWidget

--- src/main/java/hr/vsite/mentor/web/widgets/LectureWidget.java
+++ src/main/java/hr/vsite/mentor/web/widgets/LectureWidget.java
@@ -3,24 +3,18 @@ package hr.vsite.mentor.web.widgets;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
-import com.google.gwt.resources.client.ClientBundle.Source;
 import com.google.gwt.user.client.ui.Composite;
 
 import hr.vsite.mentor.lecture.Lecture;
-import hr.vsite.mentor.web.Places;
-import hr.vsite.mentor.web.theme.Theme;
-import hr.vsite.mentor.web.widgets.LectureCard.Resources;
-import hr.vsite.mentor.web.widgets.LectureCard.Style;
 
 import gwt.material.design.client.constants.Color;
-import gwt.material.design.client.constants.HideOn;
+import gwt.material.design.client.ui.MaterialChip;
 import gwt.material.design.client.ui.MaterialColumn;
 import gwt.material.design.client.ui.MaterialImage;
 import gwt.material.design.client.ui.MaterialLabel;
 import gwt.material.design.client.ui.MaterialLink;
 import gwt.material.design.client.ui.MaterialPanel;
 import gwt.material.design.client.ui.MaterialRow;
-import gwt.material.design.themes.client.ThemeLoader;
 
 public class LectureWidget extends Composite {
 
@@ -35,6 +29,7 @@ public class LectureWidget extends Composite {
 		String title();
 		String image();
 		String description();
+		String keyword();
 	}
 	
 	public LectureWidget(Lecture lecture) {
@@ -42,6 +37,7 @@ public class LectureWidget extends Composite {
 		MaterialPanel view = new MaterialPanel();
 		view.setBackgroundColor(Color.WHITE);
 		view.setShadow(1);
+		view.setHoverable(true);
 		view.addStyleName(res.style().view());
 			MaterialRow titleRow = new MaterialRow();
 			titleRow.setBackgroundColor(Color.RED_LIGHTEN_2);
@@ -57,25 +53,27 @@ public class LectureWidget extends Composite {
 			MaterialRow contentRow = new MaterialRow();
 				MaterialColumn imageColumn = new MaterialColumn();
 				imageColumn.setGrid("s12 m6 l4");
-					image = new MaterialImage();
-					image.addStyleName(res.style().image());
-					image.addStyleName(Theme.bundle().style().clickable());
-				imageColumn.add(image);
+					imageLink = new MaterialLink();
+						image = new MaterialImage();
+						image.addStyleName(res.style().image());
+					imageLink.add(image);
+				imageColumn.add(imageLink);
 			contentRow.add(imageColumn);
-				MaterialColumn descriptionColumn = new MaterialColumn();
-				descriptionColumn.setGrid("s12 m6 l8");
+				MaterialColumn descriptionKeyowordsColumn = new MaterialColumn();
+				descriptionKeyowordsColumn.setGrid("s12 m6 l8");
 					description = new MaterialLabel();
 					description.addStyleName(res.style().description());
 					description.addStyleName("flow-text");
-				descriptionColumn.add(description);
-			contentRow.add(descriptionColumn);
+				descriptionKeyowordsColumn.add(description);
+					keywordsContainer = new MaterialPanel();
+					keywordsContainer.setPaddingBottom(5.0);
+				descriptionKeyowordsColumn.add(keywordsContainer);
+			contentRow.add(descriptionKeyowordsColumn);
 		view.add(contentRow);
 		
 		initWidget(view);
 		
 		setLecture(lecture);
-		
-		//image.addClickHandler(e -> Places.controller().goTo(new LecturePlace(lecture.getId())));
 		
 	}
 
@@ -87,16 +85,27 @@ public class LectureWidget extends Composite {
 		
 		title.setText(lecture.getTitle());
 		title.setHref(lectureHref);
+		imageLink.setHref(lectureHref);
 		image.setUrl("https://www.vsite.hr/sites/default/files/promocija2015_027.JPG");	// TODO
 		description.setText(lecture.getDescription());
 //		unitsLink.setText("13 units");	// TODO
 //		unitsLink.setHref(lectureHref);
 		
+		keywordsContainer.clear();
+		if (lecture.getLectureKeywords() != null)
+			for (String keyword : lecture.getLectureKeywords()) {
+				MaterialChip chip = new MaterialChip(keyword);
+				chip.addStyleName(res.style().keyword());
+				keywordsContainer.add(chip);
+			}
+		
 	}
 
-	private final MaterialImage image;
 	private final MaterialLink title;
+	private final MaterialLink imageLink;
+	private final MaterialImage image;
 	private final MaterialLabel description;
+	private final MaterialPanel keywordsContainer;
 //	private final MaterialLink unitsLink;
 	private Lecture lecture = null;
 	
