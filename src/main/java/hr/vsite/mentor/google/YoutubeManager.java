4e5a2aed1ac7b6d8445cb86476e40ece6c177cc1
Marijo Horvat <Marijo.Horvat@vsite.hr>
2017-01-03 00:11:29 +0100

    Added a two new methods that returns an IFrame. First one with default width, height  (480 x 270) Second one with params for width and height. Return value: IFrame as a String

--- src/main/java/hr/vsite/mentor/google/YoutubeManager.java
+++ src/main/java/hr/vsite/mentor/google/YoutubeManager.java
@@ -9,17 +9,15 @@ import com.google.api.services.youtube.model.Video;
 import com.google.api.services.youtube.model.VideoListResponse;
 import com.google.gwt.core.shared.GwtIncompatible;
 
-import java.io.Console;
+
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
 import java.time.Duration;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import hr.vsite.mentor.MentorConfiguration;
-import hr.vsite.mentor.servlet.rest.providers.ExceptionMapperProvider;
+
 
 public class YoutubeManager {
 
@@ -65,12 +63,46 @@ public class YoutubeManager {
 		targetVideo = videoList.iterator().next();	
 	}
 	
+	// Default Thumbnail = 120px by 90px
 	public String GetThumbnail() {
 		return targetVideo.getSnippet().getThumbnails().getDefault().getUrl();
 	}
 		
 	public String GetDuration() {
 		return VideoDuration(targetVideo.getContentDetails().getDuration());
+	}
+	
+	public String GetTitle() {
+		return targetVideo.getSnippet().getTitle();
+	}
+	
+	// default video size 480 x 270
+	public String GetIFrame () {
+		int width = 480;
+		int height = 270;		
+		return IFrame(width, height);
+	}
+	
+	public String GetIFrame (int width, int height) {
+		return IFrame(width, height);
+	}
+	
+	// iframe id is generated from the first word of the video title 
+	private String IFrame(int width, int height) {
+		
+		StringBuilder IFrame = new StringBuilder(1000);
+		String id = GetTitle();
+		
+		if (id.indexOf(' ') > -1)
+			id = id.substring(0,id.indexOf(' '));
+		
+		IFrame.append("<iframe id=\"").append(id).append("\" ");
+		IFrame.append("type=\"text/html\" width=\"").append(width).append("\" ");
+		IFrame.append("hight=\"").append(height).append("\" ");
+		IFrame.append("src=\"https://www.youtube.com/embed/").append(videoId).append("\" ");
+		IFrame.append("frameborder=\"0\" allowfullscreen>");
+		
+		return IFrame.toString();
 	}
 	
 	// Parsing video duration output. From PT5H4M13S to 5 hrs 4 min 13 sec
@@ -82,11 +114,6 @@ public class YoutubeManager {
 			return totalSeconds/ 60 + " min " + totalSeconds % 60 + " sec";
 		return totalSeconds + " sec";
 		}
-	
-	public String GetTitle() {
-		return targetVideo.getSnippet().getTitle();
-	}
-
 	
 	private String videoId;
 	private List<Video> videoList;
